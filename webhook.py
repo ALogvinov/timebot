f3e1fe0dc23c02900ae5cb88894b2c1ae5430272
Alexander Logvinov <logvinovaleksander@gmail.com>
2023-11-30 17:59:07 +0300

    Сделали запрос времени к API

--- webhook.py
+++ webhook.py
@@ -3,17 +3,24 @@ from flask import Flask, request, jsonify, make_response, abort
 app = Flask(__name__)
 
 
+def get_time(latitude, longitude):
+    time = '09:16'
+    date = "30.11.2023"
+    return f'{date} {time}'
+
+
 def get_result():
     req = request.get_json(force=True)
-    print(req)
+    current_time = get_time(45.1, 67.2)
+    print(current_time)
+    return {
+        'fulfillmentText': current_time
+    }
 
 
 @app.route('/')
 def index():
     return 'Hello from Flask!'
-
-
-
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
